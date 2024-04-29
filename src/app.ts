c6539b17dcad97344dc149d84324c44f37bf87e2
Quenten <quentenwelch@gmail.com>
2024-04-29 17:47:08 -0600

    add cors

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
 import express from "express";
 import userRoutes from "./routes/user.routes.js";
+import cors from "cors";
 
 const app = express();
 
+
 app.use(express.json());
+app.use(cors({origin:"http://localhost:3000"}));
 
 // /api/user/register
 // /api/user/login
