855939f395a14367f2028a660be81d70b9bfac8c
Quenten <quentenwelch@gmail.com>
2024-05-03 11:44:35 -0600

    add zod schema validation

--- src/routes/chores.routes.ts
+++ src/routes/chores.routes.ts
@@ -6,11 +6,12 @@ import {
 } from "../controllers/chores.controller.js";
 import { verifyJwt } from "../middleware/auth.middleware.js";
 import { validateData } from "../middleware/validation.middleware.js";
+import { choreSchema } from "../schemas/chore.schemas.js";
 
 const router = Router();
 
 router.route("/child/:childId").get(verifyJwt, getChores);
-router.route("/create").post(verifyJwt, createChore);
+router.route("/create").post(verifyJwt, validateData(choreSchema), createChore);
 router.route("/update/:id").put(verifyJwt, updateChoreStatus);
 
 export default router;
