3fd524036c6d9ea44faa099cd78036a20151c11e
Quenten <quentenwelch@gmail.com>
2024-05-03 11:34:24 -0600

    fix routing for chores

--- src/controllers/chores.controller.ts
+++ src/controllers/chores.controller.ts
@@ -3,24 +3,32 @@ import { Request, Response } from "express";
 import { STATUS_CODES } from "../constants/enums.js";
 
 const getChores = async (req: Request, res: Response) => {
+  const { childId } = req.params;
+
   try {
     const chores = await prisma.chores.findMany({
+      where: {
+        childUserId: childId,
+      },
       include: {
         assignedTo: true,
-        assignedBy: true
-      }
+        assignedBy: true,
+      },
     });
     res.json(chores);
   } catch (error) {
     return res
-    .status(STATUS_CODES.INTERNAL_SERVER_ERROR)
-    .send("Internal server error");
+      .status(STATUS_CODES.INTERNAL_SERVER_ERROR)
+      .send("Internal server error");
   }
 };
 
 const createChore = async (req: Request, res: Response) => {
+  //get parent ID from JWT
+  const parentId = res.locals.user.id;
+
   try {
-    const { title, amount, dueDate, childUserId, parentUserId } = req.body;
+    const { title, amount, dueDate, childUserId } = req.body;
     const newChore = await prisma.chores.create({
       data: {
         title,
@@ -28,15 +36,15 @@ const createChore = async (req: Request, res: Response) => {
         dueDate,
         status: "NOT_ACCEPTED", // default status
         childUserId,
-        parentUserId
-      }
+        parentUserId: parentId,
+      },
     });
     res.status(201).json(newChore);
   } catch (error) {
     return res
-    .status(STATUS_CODES.INTERNAL_SERVER_ERROR)
-    .send("Internal server error");
-    }
+      .status(STATUS_CODES.INTERNAL_SERVER_ERROR)
+      .send("Internal server error");
+  }
 };
 
 const updateChoreStatus = async (req: Request, res: Response) => {
@@ -45,14 +53,14 @@ const updateChoreStatus = async (req: Request, res: Response) => {
   try {
     const updatedChore = await prisma.chores.update({
       where: { choreId: id },
-      data: { status }
+      data: { status },
     });
     res.json(updatedChore);
   } catch (error) {
     return res
-    .status(STATUS_CODES.INTERNAL_SERVER_ERROR)
-    .send("Internal server error");
-    }
+      .status(STATUS_CODES.INTERNAL_SERVER_ERROR)
+      .send("Internal server error");
+  }
 };
 
 export { getChores, createChore, updateChoreStatus };
--- src/routes/chores.routes.ts
+++ src/routes/chores.routes.ts
@@ -1,13 +1,15 @@
 import { Router } from "express";
-import { getChores, createChore, updateChoreStatus } from "../controllers/chores.controller.js";
+import {
+  getChores,
+  createChore,
+  updateChoreStatus,
+} from "../controllers/chores.controller.js";
 import { verifyJwt } from "../middleware/auth.middleware.js";
 import { validateData } from "../middleware/validation.middleware.js";
 
-
-
 const router = Router();
 
-router.route("/").get(verifyJwt, getChores);
+router.route("/child/:childId").get(verifyJwt, getChores);
 router.route("/create").post(verifyJwt, createChore);
 router.route("/update/:id").put(verifyJwt, updateChoreStatus);
 
