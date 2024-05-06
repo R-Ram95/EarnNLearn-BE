2eb815bfb81dfdaeb2bf329b07aa6c19ee40a10b
Quenten <quentenwelch@gmail.com>
2024-05-06 17:48:45 -0600

    add delete chore route and functionality

--- src/routes/chores.routes.ts
+++ src/routes/chores.routes.ts
@@ -3,6 +3,7 @@ import {
   getChores,
   createChore,
   updateChoreStatus,
+  deleteChore,
 } from "../controllers/chores.controller.js";
 import { verifyJwt } from "../middleware/auth.middleware.js";
 import { validateData } from "../middleware/validation.middleware.js";
@@ -13,5 +14,6 @@ const router = Router();
 router.route("/child/:childId").get(verifyJwt, getChores);
 router.route("/create").post(verifyJwt, validateData(choreSchema), createChore);
 router.route("/update/:id").put(verifyJwt, updateChoreStatus);
-
+//delete id is the chore's id
+router.route("/delete/:id").delete(verifyJwt, deleteChore);
 export default router;
